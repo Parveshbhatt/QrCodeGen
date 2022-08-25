5f30d14a49c1db7d49d995db6f9a91a56e1545f6
Parveshbhatt <parveshbhatt118@gmail.com>
2022-08-26 00:39:35 +0530

    create download button

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function genQR(){
     // Google chart API URL
-    var gapi = "https://chart.googleapis.com/chart?cht=qr&chs=";
+    
+    var gapi = "https://chart.googleapis.com/chart?chf=bg,s,65432100&cht=qr&chs=";
     var img = document.getElementById("qrimg");
     var text = document.getElementById("qrtext").value;
     var size = document.getElementById("size").value;
@@ -24,4 +25,28 @@ function genQR(){
     else{
         alert("Please Enter Text");
     }
+
+    clearUI();
+
+    // const saveUrl = img.src;
+    const saveUrl = "https://www.gndec.ac.in/gndec/gne_front.jpg";
+    createSaveBtn(saveUrl);
 }
+
+const createSaveBtn = (saveUrl) => {
+    const link = document.createElement('a');
+    link.id = 'save-link';
+    link.classList = 'downloadqr';
+    link.href = saveUrl;
+    link.download = 'qrcode';
+    // link.setAttribute('download', "download");
+    link.innerHTML = 'Save Image';
+
+    document.getElementById('generated').appendChild(link);
+};
+
+
+const clearUI = () => {
+    const saveLink = document.getElementById('save-link');
+    if(saveLink) saveLink.remove();
+};
